47515bbb1b6d9c23a3ba3cca31970a6fed5e55aa
profesor <profesor@T1PC254>
2026-02-20 19:35:17 +0000

    Primer Commit. Creacion de Repositorio.

---
+++ src/main/java/com/mycompany/ut05actividadgitide/Prueba.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ut05actividadgitide;
+
+/**
+ *
+ * @author profesor
+ */
+public class Prueba {
+public static void main(String[] args) {
+printCuenta ("Mónica",2);
+}
+private static void printCuenta (String nombre, double cantidad) {
+System.out.println ("nombre: " + nombre);
+System.out.println ("cantidad: " + cantidad);
+}
+}
